033830d1255939bf2d82316a2a55bc8a9b6af354
Tiger Kong <tig567899@users.noreply.github.com>
2020-02-06 20:42:54 -0800

    Ready for initial deployment

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,12 @@
 
 class Constants {
   static const PRODUCTION_URL = 'https://uwhvz.uwaterloo.ca/api/v1/';
-  // Change this to your testing server
-  static const TESTING_URL = 'http://tigerkong.me:7126/api/v1/';
+  // Change this to your testing server if you wish.
+  static const TESTING_URL = 'https://uwhvz.uwaterloo.ca/api/v1/';
 
-  // Twitter
+  // Twitter Keys. We're using a free API, so they're visible. Feel free to use
+  // them for your testing, or to generate your own.
+  // TODO: Generate club keys for deployment use. 
   static const TWITTER_API_KEY = "Y1KzPRQizRyKge8xkfuYrrBfc";
   static const TWITTER_API_SECRET_KEY = "FNhhLCQVEfeiHrOlTk9E52luNiGtoghnCdYVJkG0Gc6DNEQYe2";
   static const TWITTER_API_TOKEN = "751400215308017664-1yFISFLqv8kBMMNtlVfvTNdnZrFRm36";
